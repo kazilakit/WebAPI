e45035d79fb3c26158f11a6ce88ef4a5ff8f7c54
Kazi Lakit <lakit.kazi@gmail.com>
2017-09-27 18:41:55 +0600

    add project to git

---
+++ DataModel/GenericRepository/GenericRepository.cs
@@ -0,0 +1,18 @@
+﻿#region Using Namespaces... 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace DataModel.GenericRepository
+{
+    /// <summary>
+    /// Generic Repository class for Entity Operations
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public class GenericRepository<TEntity> where TEntity : class
+    {
+    }
+}
