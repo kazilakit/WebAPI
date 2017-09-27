f2114b1ede90dadec4e8126db5d9534ad33d1ab3
Kazi Lakit <lakit.kazi@gmail.com>
2017-09-27 18:57:19 +0600

    added generic class

--- DataModel/GenericRepository/GenericRepository.cs
+++ DataModel/GenericRepository/GenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿#region Using Namespaces... 
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +15,33 @@ namespace DataModel.GenericRepository
     /// <typeparam name="TEntity"></typeparam>
     public class GenericRepository<TEntity> where TEntity : class
     {
+        #region Private member variables...
+        internal WebApiDbEntities Context;
+        internal DbSet<TEntity> DbSet;
+        #endregion
+
+        #region Public Constructor... 
+        /// <summary>
+        /// Public Constructor,initializes privately declared local variables
+        /// </summary>
+        /// <param name="context"></param>
+        public GenericRepository(WebApiDbEntities context)
+        {
+            this.Context = context;
+            this.DbSet = context.Set<TEntity>();
+        }
+        #endregion
+
+        #region Public member methods...
+        /// <summary>
+        /// generic Get method for Entities
+        /// </summary>
+        /// <returns></returns>
+        public virtual IEnumerable<TEntity> Get()
+        {
+            IQueryable<TEntity> query = DbSet;
+            return query.ToList();
+        }
+        #endregion
     }
 }
