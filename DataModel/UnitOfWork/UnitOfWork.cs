b5fdf7b69762575a65fb44c14287674aed69f004
Kazi Lakit <lakit.kazi@gmail.com>
2017-09-28 18:57:33 +0600

    added unit of work

---
+++ DataModel/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿#region Using Namespaces... 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+#endregion
+
+namespace DataModel.UnitOfWork
+{
+    public class UnitOfWork:IDisposable
+    {
+        #region Private member variables...
+
+        #endregion
+
+        #region implementing Idisposal
+        #region private disposal variable
+        private bool disposed = false;
+        #endregion
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    Debug.WriteLine("UnitOfWork is being disposed");
+                }
+            }
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
